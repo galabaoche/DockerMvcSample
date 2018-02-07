9fa4183d36817c04ec73210bb56cb4da92463288
galabaoche <galabaoche@live.com>
2018-02-07 20:56:40 +0800

    add docker for coremvc

---
+++ Logging.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcSample
+{
+    public class Logging
+    {
+        public bool IncludeScopes { get; set; }
+        public Level LogLevel { get; set; }
+    }
+
+    public class Level
+    {
+        public string Default { get; set; }
+        public string System { get; set; }
+        public string Microsoft { get; set; }
+    }
+}
